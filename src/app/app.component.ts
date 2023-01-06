6a29cb526eeb5ca51c65691036116db4469140dc
Cong Dinh <dinhdc1111@gmail.com>
2023-01-06 18:42:19 +0700

    update data

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -37,6 +37,17 @@ export class AppComponent {
         'http://openclipart.org/image/300px/svg_to_png/26215/Anonymous_Leaf_Rake.png',
     },
     {
+      productId: 2,
+      productName: 'Garden Cart',
+      productCode: 'GDN-0023',
+      releaseDate: 'March 18, 2016',
+      description: '15 gallon capacity rolling garden cart',
+      price: 32.99,
+      starRating: 4.2,
+      imageUrl:
+        'http://openclipart.org/image/300px/svg_to_png/58471/garden_cart.png',
+    },
+    {
       productId: 5,
       productName: 'Hammer',
       productCode: 'TBX-0048',
