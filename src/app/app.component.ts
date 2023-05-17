b4307ec634802b0abd6b8dba9f7dcb0da3b18199
Cong Dinh <dinhdc1111@gmail.com>
2023-05-18 04:17:37 +0700

    Session 1: Learning about components

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,32 @@
 import { Component } from '@angular/core';
 
 @Component({
-  selector: 'app-root',
-  templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+    selector: 'app-root',
+    templateUrl: './app.component.html',
+    styleUrls: ['./app.component.css'],
 })
+// Here defines the components
 export class AppComponent {
-  title = 'my-app';
+    // Definition of variables, these variables can receive values ​​in html files.
+    title = 'Angular WD17302 FPT Polytechnic Ha Noi';
+    products = [
+        {
+            id: '1',
+            name: 'Product A',
+            price: 1000,
+            img: 'https://picsum.photos/200/200',
+        },
+        {
+            id: '2',
+            name: 'Product B',
+            price: 2000,
+            img: 'https://picsum.photos/200/200',
+        },
+        {
+            id: '3',
+            name: 'Product C',
+            price: 3000,
+            img: 'https://picsum.photos/200/200',
+        },
+    ];
 }
