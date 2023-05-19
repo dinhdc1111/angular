71bcdcc047def1a5246720ab4a6dfc3fa443c4ad
Dong Cong Dinh <dinhdc1111@gmail.com>
2023-05-20 05:45:51 +0700

    feat: :fire: Lession 5: Routing

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -2,14 +2,35 @@ import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { HomePageComponent } from './page/home-page/home-page.component';
 import { ProductListComponent } from './components/product-list/product-list.component';
+import { AboutPageComponent } from './page/about-page/about-page.component';
+import { UserLayoutComponent } from './layouts/user-layout/user-layout.component';
+import { AdminLayoutComponent } from './layouts/admin-layout/admin-layout.component';
+import { DashboardComponent } from './page/admin/dashboard/dashboard.component';
+import { AdminProductComponent } from './page/admin/admin-product/admin-product.component';
 
 const routes: Routes = [
-  {path: "", component: HomePageComponent},
-  {path: "products", component: ProductListComponent},
+  {
+    path: '',
+    component: UserLayoutComponent,
+    children: [
+      { path: '', component: HomePageComponent },
+      { path: 'products', component: ProductListComponent },
+      { path: 'about', component: AboutPageComponent },
+    ],
+  },
+  {
+    path: 'admin',
+    component: AdminLayoutComponent,
+    children: [
+      { path: '', redirectTo: 'dashboard', pathMatch: 'full' },
+      { path: 'dashboard', component: DashboardComponent },
+      { path: 'products', component: AdminProductComponent },
+    ],
+  },
 ];
 
 @NgModule({
-    imports: [RouterModule.forRoot(routes)],
-    exports: [RouterModule],
+  imports: [RouterModule.forRoot(routes)],
+  exports: [RouterModule],
 })
 export class AppRoutingModule {}
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -7,9 +7,28 @@ import { AppComponent } from './app.component';
 import { NgbModule } from '@ng-bootstrap/ng-bootstrap';
 import { ProductListComponent } from './components/product-list/product-list.component';
 import { HomePageComponent } from './page/home-page/home-page.component';
+import { AdminLayoutComponent } from './layouts/admin-layout/admin-layout.component';
+import { UserLayoutComponent } from './layouts/user-layout/user-layout.component';
+
+import { NotFoundPageComponent } from './page/not-found-page/not-found-page.component';
+import { ProductPageComponent } from './page/product-page/product-page.component';
+import { DashboardComponent } from './page/admin/dashboard/dashboard.component';
+import { AdminProductComponent } from './page/admin/admin-product/admin-product.component';
+import { AboutPageComponent } from './page/about-page/about-page.component';
 
 @NgModule({
-  declarations: [AppComponent, ProductListComponent, HomePageComponent],
+  declarations: [
+    AppComponent,
+    ProductListComponent,
+    HomePageComponent,
+    AdminLayoutComponent,
+    UserLayoutComponent,
+    NotFoundPageComponent,
+    ProductPageComponent,
+    DashboardComponent,
+    AdminProductComponent,
+    AboutPageComponent,
+  ],
   imports: [BrowserModule, AppRoutingModule, NgbModule, HttpClientModule],
   providers: [],
   bootstrap: [AppComponent],
